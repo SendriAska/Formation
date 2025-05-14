2f822b739ecdee65bb4c57859c2f8e1c3e797865
SendriAska <thomas.boscaro@gmail.com>
2025-05-14 17:03:46 +0200

    last push of the day

--- Algo/Exo Mystery/mystery.js
+++ Algo/Exo Mystery/mystery.js
@@ -8,7 +8,7 @@ function f1(arr) {
 	return result;
 }
 
-//
+// On retourne la somme des valeurs des cases dans lesquelles les valeurs sont paires.
 	
 function f2(arr) {
 	let result = 0;
@@ -18,7 +18,21 @@ function f2(arr) {
 		}
 	}
 	return result;
-}
+} 
+
+// On retourne la somme des valeurs des cases dont le numéro est pair.
 
 // Écrire une fonction f3 qui prend un tableau en entrée
 // et qui retourne la somme des numéros de cases des éléments pairs d'un tableau
+
+function f1(arr) {
+	let result = 0;
+	for(let i = 0 ; i < arr.length ; i++) {
+		if(arr[i]%2==0) {
+			result += i;
+		}
+	}
+	return result;
+}
+
+//
--- Algo/Exo tri/bulles.js
+++ Algo/Exo tri/bulles.js
@@ -2,8 +2,34 @@
 
 // Étape 1 : écrire une fonction qui reçoit un tableau et un numéro de case (i). Son but est d'échanger les valeurs des cases i et i+1 si celles-ci ne sont pas dans le bon ordre.
 
+let tableau = [54,2,74,1,3,9,7,54,63,9,63,665,7842,356,65,24,1,4,7,5,6322]
+
+
+function bulle (tab, i) {
+    if (tab[i]>tab[i+1]) {
+        let save = tab[i];
+        tab[i] = tab[i+1];
+        tab[i+1] = save;
+    }
+}
+
 // Étape 2 : écrire une fonction qui reçoit un tableau et qui applique l'étape 1 sur toutes les cases successives (cases 0/1, cases 1/2, cases 2/3, ..., cases avant-dernière/dernière).
 
+function bulleDeux (tab) {
+    for (let j = 0 ; j < tab.length-2 ; j++) {
+        bulle(tab,j);
+    }
+}
+
 // Étape 3 : écrire une fonction qui reçoit un tableau et qui le trie en appelant la fonction 2 autant de fois qu'il y a d'éléments dans le tableau.
+
+function bulleTrois (tab) {
+    for (let k = 0 ; k < tab.length-1; k++) {
+        bulleDeux(tab);
+    }
+}
+
+bulleTrois(tableau);
+console.log(tableau);
 
 // Note : on peut visualiser cet algorithme sur cette image <https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/211162/a9b17b54-b6dd-1602-be04-6fc125676b7a.gif>.
