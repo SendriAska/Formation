bc94a4ca37df54792c2255ff108449fa7f92500d
SendriAska <thomas.boscaro@gmail.com>
2025-06-20 16:29:31 +0200

    last commit af the week

--- JS/Exo DOM/js/main.js
+++ JS/Exo DOM/js/main.js
@@ -1,43 +1,101 @@
-// let intro = document.createElement("span");
-// let division =document.createElement("div");
+// // let intro = document.createElement("span");
+// // let division =document.createElement("div");
+
+// // let text = document.body.getElementsByTagName("p");
+// let title = document.getElementById("titre");
+// let parentTitle = document.getElementById("titre").parentNode;
+
+
+// function addIntro() {
+//     let intro = document.createElement("span");
+//     let introContent = document.createTextNode("Je suis une introduction.");
+//     intro.appendChild(introContent);
+//     let parentTitle = document.getElementById("titre").parentNode;
+//     parentTitle.insertBefore(intro, title);
+// }
+
+// document.body.onload = addIntro;
+
+
+// function createNewParag(p, m) {
+//     let text = document.body.getElementsByTagName("p");
+//     document.body.removeChild(text[p]);
+//     let textContent = document.createTextNode("Je suis un texte ajouté");
+//     let createParag = document.createElement("p");
+
+//     createParag.appendChild(textContent);
+//     document.body.replaceChild(createParag, text[m]);
+// }
+
+
+// createNewParag(3, 2)
+
+
+// function addTxt(pseudo, text) {
+//     let newTxt = document.createElement("p");
+//     newTxt.textContent = `${pseudo} , ${text}`;
+//     document.body.appendChild(newTxt);
+// }
+
+// addTxt("Bonjour", "le pain");
+
+
 
 // let text = document.body.getElementsByTagName("p");
-let title = document.getElementById("titre");
-let parentTitle = document.getElementById("titre").parentNode;
+// let texteTab = Array.from(text);
+// texteTab.map(txt => txt.textContent = "LOL HACKED");
+
+// let firstP = document.body.querySelector("p");
+// console.log(firstP.offsetHeight, firstP.offsetWidth);
 
 
-function addIntro() {
-    let intro = document.createElement("span");
-    let introContent = document.createTextNode("Je suis une introduction.");
-    intro.appendChild(introContent);
-    let parentTitle = document.getElementById("titre").parentNode;
-    parentTitle.insertBefore(intro, title);
-}
+// text[0].addEventListener("click", function () {
+//     title.classList.add("maCouleur");
+// });
 
-document.body.onload = addIntro;
+// text[1].addEventListener("click", function () {
+//     title.classList.remove("maCouleur");
+// });
 
-
-function createNewParag(p,m) {
-    let text = document.body.getElementsByTagName("p");
-    document.body.removeChild(text[p]);
-    let textContent = document.createTextNode("Je suis un texte ajouté");
-    let createParag = document.createElement("p");
-
-    createParag.appendChild(textContent);
-    document.body.replaceChild(createParag, text[m]);
-}
+// text[2].addEventListener("click", function () {
+//     title.classList.toggle("maCouleur")
+// });
 
 
-createNewParag(3,2)
+// function monClick() {
+//     console.log("Bonjour");
+//     firstP.removeEventListener("click", monClick);
+// }
 
 
-function addTxt (pseudo, text) {
-    let newTxt = document.createElement("p");
-    newTxt.textContent = `${pseudo} , ${text}` ;
-    document.body.appendChild(newTxt);
-}
+// firstP.addEventListener("click", monClick);
+// let typedText = "";
 
-addTxt("Bonjour", "le pain");
+// addEventListener("keydown", function (event) {
+//     if (event.key.length === 1) { 
+//         typedText += event.key; 
+//         firstP.textContent = typedText; 
+//     } else if (event.key === "Backspace") {
+        
+//         typedText = typedText.slice(0, -1);
+//         firstP.textContent = typedText;
+//     }
+// });
+
+
+addEventListener("click", function(unEvent) {
+    console.log(unEvent.x, unEvent.y);
+    let img = document.createElement("img");
+    let taille = 58;
+    img.setAttribute("src", `https://loremflickr.com/${taille}/${taille}`);
+
+    // style
+    img.style.position ="absolute";
+    img.style.left = unEvent.x - taille /2 + "px";
+    img.style.top = unEvent.y - taille /2 + "px";
+
+    document.body.appendChild(img);
+});
 
 
 // document.body.insertBefore(division, text[0]);
